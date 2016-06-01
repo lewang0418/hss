65036c3ddf75c8d75082347305ebe8c06480b165
ellewng <le.l.wang@ericsson.com>
2016-06-01 14:59:38 +0300

    HSS blueprint initial commit

---
+++ scripts/install-hss.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+ctx logger info "installing OpenImsCore HSS..."
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt-get update
+echo "oracle-java6-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+sudo apt-get install -y oracle-java6-installer subversion ant
+echo "mysql-server mysql-server/root_password password root" | sudo debconf-set-selections
+echo "mysql-server mysql-server/root_password_again password root" | sudo debconf-set-selections
+sudo apt-get install -y mysql-server-5.6
+
+cd /home/ubuntu
+svn checkout https://svn.code.sf.net/p/openimscore/code/FHoSS/trunk FHoSS
+cd FHoSS
+ant compile
+ant deploy
+
+FHOSS_PATH=`pwd`
+HSS_DIAMETER_FILE=$FHOSS_PATH"/deploy/DiameterPeerHSS.xml"
+HSS_PROPERTY_FILE=$FHOSS_PATH"/deploy/hss.properties"
+
+sed -i -e 's/open-ims.test/example.com/' $HSS_DIAMETER_FILE
+sed -i -e 's/open-ims.test/example.com/' $HSS_DIAMETER_FILE
+sed -i -e 's/127.0.0.1/0.0.0.0/' $HSS_DIAMETER_FILE
+sed -i -e 's/127.0.0.1/0.0.0.0/' $HSS_PROPERTY_FILE
+
+
+mysql -uroot -proot -h localhost < $FHOSS_PATH"/scripts/hss_db.sql"
+mysql -uroot -proot -h localhost < $FHOSS_PATH"/scripts/userdata.sql"
+
+ctx instance runtime-properties hss_path ${FHOSS_PATH}
+
+
---
+++ scripts/stop-hss.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+ctx logger info "stop OpenImsCore HSS..."
+
+set -e
+
+PID=$(ctx instance runtime_properties hss_pid)
+
+kill -9 ${PID}
+
+ctx logger info "Sucessfully stopped OpenImsCore HSS (${PID})"
+
+retries=0
+cat > /home/ubuntu/dnsupdatefile << EOF
+server ${dns_ip}
+zone example.com
+key example.com 8r6SIIX/cWE6b0Pe8l2bnc/v5vYbMSYvj+jQPP4bWe+CXzOpojJGrXI7iiustDQdWtBHUpWxweiHDWvLIp6/zw==
+update delete hss.example.com. 30 A ${public_ip}
+send
+EOF
+
+while ! { sudo nsupdate /home/ubuntu/dnsupdatefile
+} && [ $retries -lt 10 ]
+do
+  retries=$((retries + 1))
+  echo 'nsupdate failed - retrying (retry '$retries')...'
+  ctx logger info "nsupdate failed retrying..."
+  sleep 5
+done
